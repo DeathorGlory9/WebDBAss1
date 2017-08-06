410e1ce76d59e97202f7947d15a461b3f7a35e86
jack <jckhayes1@gmail.com>
2017-08-06 11:03:57 +1000

    Header template

--- webfiles/resources/views/includes/header.blade.php
+++ webfiles/resources/views/includes/header.blade.php
@@ -1,17 +1,19 @@
-<div class="navbar">
+<div class="navbar content-container">
 	<a id="logo" href="/">
 		<img src='../public/images/logo.jpg' width="200">
 	</a>
-	<a>
-		Login
+	<a id="login">
+		<h3>Login</h3>
 	</a>
 </div>
 <div class="navbar navbar-static-top navbar-inverse">
-    <ul class="nav navbar-nav">
-        <li><a href="index">Home</a></li>
-        <li><a href="faq">FAQ</a></li>
-        <li><a href="submitticket">Submit Ticket</a></li>
-        <li><a href="viewticket">View Ticket</a></li>
-		<li><a href="its">ITS View</a></li>
-    </ul>
+	<div class="content-container">
+	    <ul class="nav navbar-nav">
+	        <li><a href="index">Home</a></li>
+	        <li><a href="faq">FAQ</a></li>
+	        <li><a href="submitticket">Submit Ticket</a></li>
+	        <li><a href="viewticket">View Ticket</a></li>
+			<li><a href="its">ITS View</a></li>
+	    </ul>
+	</div>
 </div>
--- webfiles/resources/views/layouts/default.blade.php
+++ webfiles/resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
     @include('includes.head')
 </head>
 <body>
-<div class="container">
+<div class="container-fluid">
 
     <header class="row">
         @include('includes.header')
